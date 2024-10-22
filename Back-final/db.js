bdd939a3a663ab03943ad4c13eac32af2d69cf90
franchescafernanda <franchescafer26@gmail.com>
2024-10-22 00:58:04 -0300

    Modify backend

--- Back-final/db.js
+++ Back-final/db.js
@@ -66,12 +66,12 @@ CREATE TABLE if not exists detalle_pedidos (
 
 const insert =() => {
   return `INSERT INTO productos (nombre, precio, descripcion, image_url) VALUES 
-('Cama para perro', 12000, 'Cama acolchonada, 100% algodón','https://dojiw2m9tvv09.cloudfront.net/11787/product/dogitcamarectangular7043.jpg'), 
-('Juguete para gato', 8000, 'Juguete interactivo con plumas','https://ss345.liverpool.com.mx/xl/1103149122.jpg'),
-('Juguete', 3500, 'Juguete para perro','https://space-theprofit.nyc3.cdn.digitaloceanspaces.com/public/Products/juguete-para-perros-forma-de-hueso_6579285a435f1.webp'),
-('Rascador', 10000, 'Juguete para gato','https://http2.mlstatic.com/D_NQ_NP_896438-MLU76434050926_052024-O.webp'),
-('Peinetas', 5000, 'Juguete para perro','https://space-theprofit.nyc3.cdn.digitaloceanspaces.com/public/Products/pack-de-accesorios-de-aseo-para-mascotas_6581a594360b5.webp'),
-('Arnes para conejo', 20000, 'Accesorio para conejos','https://faunasalud.cl/wp-content/uploads/2022/11/137-0103.jpg');`
+('Bluza juvenil', 8000, 'Bluza de dama, 100% algodón','https://yoyojeans.vteximg.com.br/arquivos/ids/188111/BLUSA-32137204-AZUL-EST_1.jpg?v=638445751310300000?1728345600086'), 
+('Abrigo dama', 15000, 'Abrigo color beigh','https://http2.mlstatic.com/D_NQ_NP_860702-CBT71844186113_092023-O.webp'),
+('Tacon', 20000, 'Tacones mujer, tono verde','https://weide.cl/cdn/shop/products/P-GH52Z_verde-1.webp?v=1678250556&width=1500'),
+('Mini-Falda', 6500, 'Falda corta, material 100% algodón','https://imagedelivery.net/4fYuQyy-r8_rpBpcY7lH_A/falabellaCO/120471537_01/w=800,h=800,fit=pad'),
+('Zapatillas', 15000, 'Zapatillas con plataforma','https://www.dimarsa.cl/media/catalog/product/m/a/marcasskechers73690-crl-rosado1jpeg_0_6.jpg'),
+('Conjunto dama', 11000, 'Conjunto negro sexy','https://faunasalud.cl/wp-content/uploads/2022/11/137-0103.jpg');`
 }
 
 const data_init_base=async() => {
