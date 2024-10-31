e488752804d2745b6a58dd55ca07b8ddf7ca10a3
franchescafernanda <franchescafer26@gmail.com>
2024-10-31 01:34:48 -0300

    modify database

--- Back-final/db.js
+++ Back-final/db.js
@@ -9,75 +9,70 @@ const pool = new Pool({
   port: process.env.DB_PORT,
 });
 
-pool.connect(function(err,client,done){
-  if (err) console.log (err)
-})
+// Función para crear tablas
+const createTables = async () => {
+  const createTableQuery = `
+    CREATE TABLE IF NOT EXISTS usuarios (
+      id_user SERIAL PRIMARY KEY,
+      nombre VARCHAR(100) NOT NULL,
+      apellido VARCHAR(100) NOT NULL,
+      email VARCHAR(100) UNIQUE NOT NULL,
+      password VARCHAR(255) NOT NULL
+    );
 
-const create_tables=() => {
-  return `CREATE TABLE if not exists usuarios (
-  id_user SERIAL PRIMARY KEY,
-  nombre VARCHAR(100) NOT NULL,
-  apellido VARCHAR(100) NOT NULL,
-  email VARCHAR(100) UNIQUE NOT NULL,
-  contraseña VARCHAR(255) NOT NULL
-);
+    CREATE TABLE IF NOT EXISTS perfiles (
+      id_perfil SERIAL PRIMARY KEY,
+      nombre VARCHAR(100) NOT NULL,
+      apellido VARCHAR(100) NOT NULL,
+      email VARCHAR(100) UNIQUE NOT NULL,
+      password VARCHAR(255) NOT NULL
+    );
 
-CREATE TABLE if not exists perfiles (
-  id_perfil SERIAL PRIMARY KEY,
-  nombre VARCHAR(100) NOT NULL,
-  apellido VARCHAR(100) NOT NULL,
-  email VARCHAR(100) UNIQUE NOT NULL,
-  contraseña VARCHAR(255) NOT NULL
-);
+    CREATE TABLE IF NOT EXISTS formularios (
+      id_form SERIAL PRIMARY KEY,
+      id_perfil INTEGER REFERENCES perfiles(id_perfil),
+      nombre VARCHAR(100) NOT NULL,
+      apellido VARCHAR(100) NOT NULL,
+      email VARCHAR(100) NOT NULL,
+      image BYTEA,
+      descripcion TEXT
+    );
 
-CREATE TABLE if not exists formularios (
-  id_form SERIAL PRIMARY KEY,
-  id_perfil INTEGER REFERENCES perfiles(id_perfil),
-  nombre VARCHAR(100) NOT NULL,
-  apellido VARCHAR(100) NOT NULL,
-  email VARCHAR(100) NOT NULL,
-  image BYTEA,
-  descripcion TEXT
-);
+    CREATE TABLE IF NOT EXISTS productos (
+      id_productos SERIAL PRIMARY KEY,
+      nombre VARCHAR(100) NOT NULL UNIQUE,
+      precio DECIMAL NOT NULL,
+      descripcion TEXT,
+      image_url VARCHAR(255)
+    );
 
-CREATE TABLE if not exists productos (
-  id_productos SERIAL PRIMARY KEY,
-  nombre VARCHAR(100) NOT NULL UNIQUE,
-  precio DECIMAL NOT NULL,
-  descripcion TEXT,
-  image_url VARCHAR(255)
-);
+    CREATE TABLE IF NOT EXISTS pedidos (
+      pedido_id SERIAL PRIMARY KEY,
+      cliente_id INTEGER REFERENCES usuarios(id_user),
+      fecha_pedido DATE NOT NULL,
+      total DECIMAL NOT NULL
+    );
 
-CREATE TABLE if not exists pedidos (
-  pedido_id SERIAL PRIMARY KEY,
-  cliente_id INTEGER REFERENCES usuarios(id_user),
-  fecha_pedido DATE NOT NULL,
-  total DECIMAL NOT NULL
-);
+    CREATE TABLE IF NOT EXISTS detalle_pedidos (
+      detalle_id SERIAL PRIMARY KEY,
+      pedido_id INTEGER REFERENCES pedidos(pedido_id),
+      producto_id INTEGER REFERENCES productos(id_productos),
+      cantidad INTEGER NOT NULL,
+      precio_unitario DECIMAL NOT NULL
+    );
+  `;
 
-CREATE TABLE if not exists detalle_pedidos (
-  detalle_id SERIAL PRIMARY KEY,
-  pedido_id INTEGER REFERENCES pedidos(pedido_id),
-  producto_id INTEGER REFERENCES productos(id_productos),
-  cantidad INTEGER NOT NULL,
-  precio_unitario DECIMAL NOT NULL
-);`
-}
+  try {
+    await pool.query(createTableQuery);
+    console.log("Tablas creadas o ya existen.");
+  } catch (err) {
+    console.error("Error creando tablas:", err);
+  }
+};
 
-const insert =() => {
-  return `INSERT INTO productos (nombre, precio, descripcion, image_url) VALUES 
-('Bluza juvenil', 8000, 'Bluza de dama, 100% algodón','https://yoyojeans.vteximg.com.br/arquivos/ids/188111/BLUSA-32137204-AZUL-EST_1.jpg?v=638445751310300000?1728345600086'), 
-('Abrigo dama', 15000, 'Abrigo color beigh','https://http2.mlstatic.com/D_NQ_NP_860702-CBT71844186113_092023-O.webp'),
-('Tacon', 20000, 'Tacones mujer, tono verde','https://weide.cl/cdn/shop/products/P-GH52Z_verde-1.webp?v=1678250556&width=1500'),
-('Mini-Falda', 6500, 'Falda corta, material 100% algodón','https://imagedelivery.net/4fYuQyy-r8_rpBpcY7lH_A/falabellaCO/120471537_01/w=800,h=800,fit=pad'),
-('Zapatillas', 15000, 'Zapatillas con plataforma','https://www.dimarsa.cl/media/catalog/product/m/a/marcasskechers73690-crl-rosado1jpeg_0_6.jpg'),
-('Conjunto dama', 11000, 'Conjunto negro sexy','https://imagedelivery.net/4fYuQyy-r8_rpBpcY7lH_A/falabellaCL/128835811_01/w=1500,h=1500,fit=pad');`
-}
+// Inicializar la base de datos
+const data_init_base = async () => {
+  await createTables();
+};
 
-const data_init_base=async() => {
-  await pool.query(create_tables())
-  await pool.query(insert())
-}
-
-
-module.exports ={data_init_base,pool}
+module.exports = { data_init_base, pool };
--- Back-final/index.js
+++ Back-final/index.js
@@ -1,38 +1,171 @@
-require('dotenv').config();
 const express = require('express');
-const app = express();
-const cors = require('cors');
-const morgan = require('morgan');
-const dotenv = require('dotenv');
+const cors = require('cors'); 
+const { pool } = require('./db'); 
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+require('dotenv').config()
 
-dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
 app.use(cors());
-app.use(morgan('dev'));
 app.use(express.json());
 
-const {pool, data_init_base} = require('./db');
-app.use((req, res, next) => {
-  req.db = pool;
-  next();
+
+
+//PRODUCTOS
+
+// Endpoint para obtener todos los productos
+app.get('/api/productos', async (req, res) => {
+    try {
+        const result = await pool.query('SELECT * FROM productos');
+        res.status(200).json(result.rows);
+    } catch (error) {
+        console.error('Error al obtener productos:', error);
+        res.status(500).json({ error: 'Error al obtener productos' });
+    }
 });
 
-const apiRoutes = require('./src/routes/apiRoutes');
-app.use('/api', apiRoutes);
 
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send('Something broke!');
+// Endpoint para obtener un producto por ID
+app.get('/api/productos/:id', async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        const result = await pool.query('SELECT * FROM productos WHERE id_productos = $1', [id]);
+        
+        if (result.rows.length === 0) {
+            return res.status(404).json({ error: 'Producto no encontrado' });
+        }
+
+        res.status(200).json(result.rows[0]);
+    } catch (error) {
+        console.error('Error al obtener producto:', error);
+        res.status(500).json({ error: 'Error al obtener producto' });
+    }
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, async() => {
-  try {
-    await data_init_base()
-    console.log(`Server running on port ${PORT}`)
-  } catch (error) {
-    console.log(error)
-  } 
-  ;
+
+
+// Endpoint para agregar un nuevo producto
+app.post('/api/productos', async (req, res) => {
+    const { nombre, precio, descripcion, image_url } = req.body;
+
+    try {
+        const result = await pool.query(
+            'INSERT INTO productos (nombre, precio, descripcion, image_url) VALUES ($1, $2, $3, $4) RETURNING *',
+            [nombre, precio, descripcion, image_url]
+        );
+        
+        res.status(201).json(result.rows[0]);
+    } catch (error) {
+        console.error('Error al agregar producto:', error);
+        if (error.code === '23505') { 
+            return res.status(409).json({ error: 'El producto ya existe.' });
+        }
+        res.status(500).json({ error: 'Error al agregar producto.' });
+    }
 });
 
-module.exports = app;
+
+
+
+
+// USUARIOS
+
+
+// Middleware de autenticación
+const authenticateToken = (req, res, next) => {
+    const token = req.headers['authorization'] && req.headers['authorization'].split(' ')[1]
+    if (token) {
+        jwt.verify(token, process.env.SECRET_KEY, (err, user) => {
+            if (err) return res.sendStatus(403)
+            req.user = user
+            next()
+        })
+    } else {
+        res.sendStatus(401)
+    }
+}
+
+// Endpoint para registrar un nuevo usuario
+app.post('/api/auth/register', async (req, res) => {
+    const { nombre, apellido, email, password } = req.body
+
+    if (!nombre || !apellido || !email || !password) {
+        return res.status(400).json({ message: 'Todos los campos son obligatorios' })
+    }
+
+    try {
+        const existingUser = await pool.query('SELECT * FROM usuarios WHERE email = $1', [email])
+        if (existingUser.rows.length > 0) {
+            return res.status(400).json({ error: 'El correo ya está en uso' })
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10)
+
+        const result = await pool.query(
+            'INSERT INTO usuarios (nombre, apellido, email, password) VALUES ($1, $2, $3, $4) RETURNING *',
+            [nombre, apellido, email, hashedPassword]
+        )
+
+        const newUser = result.rows[0]
+        const token = jwt.sign({ id: newUser.id_user, email: newUser.email }, process.env.SECRET_KEY)
+
+        res.status(201).json({ token, user: { id: newUser.id_user, nombre: newUser.nombre, apellido: newUser.apellido, email: newUser.email } })
+        /* console.log('newUser --->', newUser); */
+    } catch (error) {
+        console.error('Error al registrar usuario:', error)
+        res.status(500).json({ error: 'Error al registrar el usuario' })
+    }
+})
+
+// Endpoint para iniciar sesión
+app.post('/api/auth/login', async (req, res) => {
+    const { email, password } = req.body
+
+    try {
+        const userResult = await pool.query('SELECT * FROM usuarios WHERE email = $1', [email])
+        const user = userResult.rows[0]
+
+        if (!user) {
+            return res.status(400).json({ error: 'Credenciales incorrectas' })
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, user.password)
+        if (!isPasswordValid) {
+            return res.status(400).json({ error: 'Credenciales incorrectas' })
+        }
+
+        const token = jwt.sign({ id: user.id_user, email: user.email }, process.env.SECRET_KEY)
+
+        res.status(200).json({ token, user: { id: user.id_user, nombre: user.nombre, apellido: user.apellido, email: user.email } })
+    } catch (error) {
+        console.error('Error al iniciar sesión:', error)
+        res.status(500).json({ error: 'Error al iniciar sesión' })
+    }
+})
+
+//Endpoint para capturar los datos del usuario
+app.get('/api/auth/user', authenticateToken, async (req, res) => {
+    try {
+        const userId = req.user.id; 
+        const result = await pool.query('SELECT id_user, nombre, apellido, email FROM usuarios WHERE id_user = $1', [userId]);
+        
+        if (result.rows.length === 0) {
+            return res.status(404).json({ error: 'Usuario no encontrado' });
+        }
+
+        res.status(200).json(result.rows[0]);
+    } catch (error) {
+        console.error('Error al obtener los datos del usuario:', error);
+        res.status(500).json({ error: 'Error al obtener los datos del usuario' });
+    }
+})
+
+
+// Iniciar el servidor
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
