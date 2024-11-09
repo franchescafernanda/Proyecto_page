dbba66129596b7b165e1f744427d2214cfb9cdbf
franchescafernanda <franchescafer26@gmail.com>
2024-11-09 20:29:23 -0300

    modifying

--- Front-final/vite.config.js
+++ Front-final/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   server: {
-    port: process.env.PORT || 10000,  // Render espera que sea 10000
+    port: process.env.PORT || 3000,  // Render espera que sea 10000
     host: '0.0.0.0',  // Esto asegura que la aplicación sea accesible desde el exterior
   },
 });
